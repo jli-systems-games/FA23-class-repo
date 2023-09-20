8a9acf7a8e6e24fb4415a5c5280b1070479c5862
Luke Kim <105609017+lukekim803@users.noreply.github.com>
2023-09-20 16:54:25 -0400

    .

--- Unity Demo 2D/Assets/Scripts/Weapon.cs
+++ Unity Demo 2D/Assets/Scripts/Weapon.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+//(CreateAssetMenu)
+//(CreateAssetMenu(Weapon))
+public class Weapon : ScriptableObject
 {
    
     public enum WeaponStats
