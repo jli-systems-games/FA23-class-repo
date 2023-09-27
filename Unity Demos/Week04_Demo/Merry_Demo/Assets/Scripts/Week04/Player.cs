4163d60d238191e8971399a7210697ae387a64f4
mhosster <46171808+HHOOSSPPEE@users.noreply.github.com>
2023-09-26 21:09:14 -0400

    Week04

---
+++ Unity Demos/Week04_Demo/Merry_Demo/Assets/Scripts/Week04/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Player 
+{
+    public enum Stat
+    {
+        Strength,
+        Dexterity,
+        Constitution,
+        Intelligence,
+        Wisdom,
+        Charisma
+    }
+
+    public static Dictionary<Stat, int> playerStats = new Dictionary<Stat, int>();
+    public static string playerName = " ";
+    public static Weapon playerWeapon = null;
+
+}
---
+++ Unity Demos/Week04_Demo/Merry_Demo/Assets/Scripts/Week04/UI_CharacterCreator.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_CharacterCreator : MonoBehaviour
+{
+    //UI References
+    public int pointsRemaining;
+    public TextMeshProUGUI pointsRemainingTMP;
+    public TMP_Dropdown weaponDropdown;
+    public GameObject weaponError;
+    public Button btn_startGame;
+
+    //Weapon Setup
+    public Weapon[] startWeapons = new Weapon[0];
+    public Weapon selectedWeapon = null;
+
+    private bool _weaponCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointsRemainingTMP.text = pointsRemaining.ToString();
+
+        _weaponCheck = true;
+        weaponError.SetActive(false);
+        btn_startGame.interactable = false;
+
+        Player.playerWeapon = startWeapons[0];
+        selectedWeapon = startWeapons[0];
+    }
+
+    public void CheckWeapon()
+    {
+        string currentWeapon = weaponDropdown.options[weaponDropdown.value].text.ToString();
+        _weaponCheck = true;
+        weaponError.SetActive(false);
+
+        foreach (Weapon weapon in startWeapons) //for (int i= 0; i< startWeapons.length
+        {
+            if(weapon.weaponName == currentWeapon)
+            {
+                selectedWeapon = weapon;
+                break;
+            }
+        }
+
+        if (Player.playerStats[Player.Stat.Strength] < selectedWeapon.reqStr)
+            _weaponCheck = false;
+        if (Player.playerStats[Player.Stat.Dexterity] < selectedWeapon.reqDex)
+            _weaponCheck = false;
+        if (Player.playerStats[Player.Stat.Constitution] < selectedWeapon.reqCon)
+            _weaponCheck = false;
+        if (Player.playerStats[Player.Stat.Intelligence] < selectedWeapon.reqInt)
+            _weaponCheck = false;
+        if (Player.playerStats[Player.Stat.Wisdom] < selectedWeapon.reqWis)
+            _weaponCheck = false;
+        if (Player.playerStats[Player.Stat.Charisma] < selectedWeapon.reqCha)
+            _weaponCheck = false;
+
+        if (_weaponCheck == false)
+        {
+            weaponError.SetActive (true);
+            btn_startGame.interactable = false;
+        }
+
+    }
+
+    public void AdjustPoints(int point)
+    {
+        pointsRemaining -= point;
+        pointsRemainingTMP.text = pointsRemaining.ToString();
+
+    }
+}
