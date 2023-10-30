862f30e6d8f339b7082981ab89c23408ce45e241
Isaac <isaactian.contact@gmail.com>
2023-10-30 15:32:42 -0400

    shatter mirror sim

---
+++ InViewMirror/Assets/Scripts/MirrorEnding.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorEnding : MonoBehaviour
+{
+    public GameObject mirrorWhole;
+    public ArrayList[] mirrorPieces;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        startEnding();
+    }
+
+    void startEnding()
+    {
+        mirrorWhole.SetActive(false);
+        
+    }
+}
